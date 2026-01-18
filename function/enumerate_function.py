18ae325d5085d0ba25bc8e3a8f64b13143a6cd89
Prajwal Pathak <prajwalpathak3@gmail.com>
2026-01-18 21:20:46 +0545

    enumerate function added

---
+++ function/enumerate_function.py
@@ -0,0 +1,4 @@
+marks = [99,87,0,15,10,59,35,74]
+
+for index,mark in enumerate(marks,start=1): # start=1 simply changes the displayed index but the iteration doesnt start from 1
+    print(f"Marks at index {index} is {mark}")
--- function/fibonacci.py
+++
@@ -1,9 +0,0 @@
-
-def fibonacci(n):
-    num = int(input("Enter the no of elements to be printed: "))
-
-    
-
-
-    f1 = 0 
-    f2 = 1 
