932dfe802c77f97c6506b9975121a8d182271f50
Prajwal Pathak <prajwalpathak3@gmail.com>
2026-01-22 21:43:48 +0545

    file handling

--- file_handling/writing.py
+++ file_handling/writing.py
@@ -1,3 +1,8 @@
 
+#write mode creates new file if the file isnt present
+
+# when with statement is used there is not need to close the file, it is closed auto
+
 with open("/home/prajwalmac/python_self/file_handling/intro.txt","w") as f:
     f.write("Hi , Myself Prajwal ")
+
